693747aa08856fcf31de3e77b76dc719baabda6c
Sasha <sashabalandina2003@gmail.com>
2022-07-13 22:36:16 +0300

    feat: weather api integration, add search form functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-
+//getting elements
 const dateHeading = document.querySelector('#current-date');
+
+const searchForm = document.querySelector('#search-forecast');
+const searchedCity = document.querySelector('#searched-city');
+
+const weatherTemp = document.querySelector('#current-display-temp');
+const weatherDesc = document.querySelector('#current-display-desc');
+const humidity = document.querySelector('#humidity');
+const wind = document.querySelector('#wind');
+const feelsLike = document.querySelector('#feels-like');
+const visibility = document.querySelector('#visibility');
+const minTemp = document.querySelector('#temp-low');
+const maxTemp = document.querySelector('#temp-high');
+
+//creating a variable that will store the name of the city
+let city;
+
+//getting API key
+const apiKey = 'da02e7bd1e86c2fd40c97607f3f77f84';
 
 //setting current date
 const setDate = () => {
@@ -10,3 +28,27 @@ const setDate = () => {
 }
 
 setDate();
+
+//weather API integration
+const displayWeather = (response) => {
+    let temp = Math.round(response.data.main.temp);
+    searchedCity.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+    weatherTemp.innerHTML = `${temp}°`
+    weatherDesc.innerHTML = `${response.data.weather[0].main}`;
+    maxTemp.innerHTML = `${Math.round(response.data.main.temp_max)}`;
+    minTemp.innerHTML = `${Math.round(response.data.main.temp_min)}`;
+
+    //more info block
+    feelsLike.innerHTML = `${Math.round(response.data.main.feels_like)}`;
+    humidity.innerHTML = `${response.data.main.humidity}`;
+    wind.innerHTML = `${Math.round(response.data.wind.speed)}`;
+    visibility.innerHTML = `${response.data.visibility / 1000}`;
+}
+
+//working with search form
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchInput = document.querySelector('.form-input');
+    city = `${searchInput.value}`;
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`).then(displayWeather);
+})
